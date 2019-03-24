4e96d78666ea851f85dece5c571f499b90133f83
铁腕阿童木 <zhengzehao@maka.im>
2019-03-24 16:56:17 +0800

    添加reducer和action

---
+++ src/actions/index.js
@@ -0,0 +1,47 @@
+let nextTodoId = 0
+//因为在reducer中还要使用type，所以这里要在actionType中统一定义
+import {ADD_TODO,TOGGLE_TODO,SET_FILTER,SET_TODO_TEXT} from './actionTypes'
+/**
+ * 新增待办事项
+ */
+export const addTodo = (text)=>(
+    {
+        type:ADD_TODO,
+        id:nextTodoId++,
+        text
+    }
+)
+
+/**
+ * 切换完成状态
+ * @param {*} id 
+ */
+export const toggleTodo = (id)=>(
+    {
+        type:TOGGLE_TODO,
+        id
+    }
+)
+
+
+/**
+ * 设置过滤状态
+ * @param {*} filter 
+ */
+export const setFilter = filter =>(
+    {
+        type:SET_FILTER,
+        filter
+    }
+)
+
+/**
+ * 设置新增待办事项的文本
+ * @param {*} text 
+ */
+export const setTodoText = text =>(
+    {
+        type:SET_TODO_TEXT,
+        text
+    }
+)
